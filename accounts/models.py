3cc0aac7bba5111f7d65b72ddfe267fcc7a1d8a4
Rohit Yadav <rohittyadavvvv@gmail.com>
2025-07-24 14:27:36 +0530

    Fix: restore __init__.py in migrations folders and clean up for fresh migrations

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.models import User
 
 # Custom User Model
 class CustomUser(AbstractUser):
